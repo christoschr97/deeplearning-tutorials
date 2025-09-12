91f36aa916368408614de302106739b3f5b50291
christoschr97 <christoschristodoulou3@gmail.com>
2025-09-12 15:37:16 +0300

    add a torch tutorial with mnist

---
+++ utils/device.py
@@ -0,0 +1,10 @@
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        return torch.device('mps')
+    else:
+        return torch.device('cpu')
+
+device = get_device()
+device
